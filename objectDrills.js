bca0f46a1f347926a7c9a7e061fb0a493c9cce81
Nathan Leung <nath.leung@gmail.com>
2019-09-18 11:46:44 -0700

    Iterating over an object's properties

--- objectDrills.js
+++ objectDrills.js
@@ -8,3 +8,15 @@ let loaf = {
 
 console.log(loaf.flour, loaf.water);
 console.log(loaf.hydration());
+
+let obj = {
+    foo : 'a',
+    bar : 'b',
+    fum : 'c',
+    quux : 'd', 
+    spam : 'e'
+};
+
+for(let item in obj){
+    console.log(item, obj[item]);
+}
