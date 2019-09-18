17c28647b3dc3ff459d04c7e141c08c065502217
Nathan Leung <nath.leung@gmail.com>
2019-09-18 14:27:05 -0700

    end of day commit

--- objectDrills.js
+++ objectDrills.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let loaf = {
     flour : 300, 
     water : 210,
@@ -63,21 +65,81 @@ let cipher = {
 }
 
 function decode(str) {
-    let result = [];
+    let result = '';
     let words = str.split(' ');
     for (let i=0; i < words.length; i++) {
         let firstLetter = words[i][0];
-        for (let j=0; j< Object.keys(cipher).length; j++) {
-            if (firstLetter == Object.keys(cipher)[j]) {
-                result.push(words[i][cipher[Object.keys(cipher)[j]]-1]);
-            }
+        if(cipher[firstLetter]){
+            result += words[i][cipher[firstLetter]-1];
         }
-        result.push(' ');
+        else
+            result +=' ';
+        
+    };
+    return result;
+}
+
+
+function decodeWords(messageString, decode) {
+    return decode(messageString);
+}
+
+console.log(decodeWords(encodedMessage,decode));
+
+
+function createCharacter(name, nickName, race, origin, attack, defense, weapon){
+
+    return {
+        name : name, 
+        nickname : nickName, 
+        race : race, 
+        origin : origin,
+        attack : attack, 
+        defense : defense, 
+        weapon : weapon,
+        describe : () => {
+            console.log(`${name} is a ${race} from ${origin} ${weapon}`);
+        },
+        evaluateFight : (character) => {
+            let x = this.attack - character.defense;
+            let y = character.attack - this.defense;
+            if (x < 0 ){
+                x = 0;
+            }
+            if (y < 0){
+                y = 0;
+            }
+            console.log(`Your opponent takes ${x} damage and you receive ${y} damage`);
+        }
     };
 }
-console.log(decode(encodedMessage));
 
-// function decodeWords(messageString, decode) {
+let characters = [createCharacter('Gandalf the white', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+                  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+                  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+                  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedian', 6, 8),
+                  createCharacter('Legolas', 'logolas', 'Elf', 'Woodland Realm', 8, 5)];
 
-// }
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half - Elf', 'Rivendell', 10, 10 ));
 
+characters.find((character) => { return character.nickname === 'aragorn'}).describe();
+let hobbits = characters.filter((character) =>{
+    return character.race ==='Hobbit'
+});
+
+console.log(hobbits[0],hobbits[1]);
+
+let aboveFive = characters.filter((character) => {
+    return character.attack > 5 
+});
+
+console.log(aboveFive[0],aboveFive[1],aboveFive[2]);
+
+// characters[0].weapon = 'Wizard staff';
+// characters[0]['race'] = 'elf';
+
+let gandalf = characters.find((character) => { return character.nickname === 'gandalf'});
+
+gandalf.race = 'elf';
+
+console.log(characters[0].race);
